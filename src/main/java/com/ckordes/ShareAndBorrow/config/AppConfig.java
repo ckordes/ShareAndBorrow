5d948381df790bcce5048842c0202c567329173a
ckordes <ckordes@gmail.com>
2020-01-26 23:46:56 +0100

    app config

---
+++ src/main/java/com/ckordes/ShareAndBorrow/config/AppConfig.java
@@ -0,0 +1,37 @@
+package com.ckordes.ShareAndBorrow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/403").setViewName("403");
+    }
+    @Bean
+    public JavaMailSenderImpl mailSender(){{
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        return javaMailSender;
+    }}
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+
+
+
+}
